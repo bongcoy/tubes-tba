a2b09207c6e8648b76146d93951814cf3c32004e
bongcoy <mpascaldewantara@gmail.com>
2022-06-08 18:19:00 +0700

    add result_tile

---
+++ lib/widgets/result_tile.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class ResultTile extends StatelessWidget {
+  final String title;
+  final bool result;
+
+  const ResultTile({Key? key, required this.title, required this.result})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: result
+          ? const Icon(
+              Icons.check,
+              color: Colors.green,
+            )
+          : const Icon(
+              Icons.clear,
+              color: Colors.red,
+            ),
+      title: Text(title),
+      trailing: Text(result.toString()),
+    );
+  }
+}
