e804c8b17cd739527346cbf5ad67296736a8c35b
bongcoy <mpascaldewantara@gmail.com>
2022-06-08 09:14:17 +0700

    update true FA

--- lib/providers/finite_automata.dart
+++ lib/providers/finite_automata.dart
@@ -13,41 +13,40 @@ class FiniteAutomata with ChangeNotifier {
     addTransition(FAState.q0, "s", FAState.q1);
     addTransition(FAState.q1, "a", FAState.q2);
 
-    addTransition(FAState.q2, "k", FAState.q2);
-    addTransition(FAState.q2, "o", FAState.q2);
+    addTransition(FAState.q0, "k", FAState.q4);
+    addTransition(FAState.q4, "o", FAState.q2);
 
-    addTransition(FAState.q2, "i", FAState.q2);
-    addTransition(FAState.q2, "t", FAState.q2);
-    addTransition(FAState.q2, "o", FAState.q2);
-    addTransition(FAState.q2, "r", FAState.q2);
-    addTransition(FAState.q2, "a", FAState.q2);
-    addTransition(FAState.q2, "n", FAState.q2);
-    addTransition(FAState.q2, "g", FAState.q2);
+    addTransition(FAState.q4, "i", FAState.q5);
+    addTransition(FAState.q5, "t", FAState.q6);
+    addTransition(FAState.q6, "o", FAState.q7);
+    addTransition(FAState.q7, "r", FAState.q8);
+    addTransition(FAState.q8, "a", FAState.q9);
+    addTransition(FAState.q9, "n", FAState.q10);
+    addTransition(FAState.q10, "g", FAState.q2);
 
-    addTransition(FAState.q2, "m", FAState.q2);
-    addTransition(FAState.q2, "a", FAState.q2);
-    addTransition(FAState.q2, "k", FAState.q2);
+    addTransition(FAState.q0, "m", FAState.q11);
+    addTransition(FAState.q11, "a", FAState.q12);
+    addTransition(FAState.q12, "k", FAState.q8);
 
-    addTransition(FAState.q2, "i", FAState.q2);
-    addTransition(FAState.q2, "n", FAState.q2);
+    addTransition(FAState.q12, "i", FAState.q13);
+    addTransition(FAState.q13, "n", FAState.q2);
 
-    addTransition(FAState.q2, "n", FAState.q2);
-    addTransition(FAState.q2, "a", FAState.q2);
-    addTransition(FAState.q2, "e", FAState.q2);
+    addTransition(FAState.q0, "n", FAState.q14);
+    addTransition(FAState.q14, "a", FAState.q15);
+    addTransition(FAState.q15, "e", FAState.q2);
 
-    addTransition(FAState.q2, "b", FAState.q2);
-    addTransition(FAState.q2, "o", FAState.q2);
-    addTransition(FAState.q2, "l", FAState.q2);
-    addTransition(FAState.q2, "a", FAState.q2);
+    addTransition(FAState.q0, "b", FAState.q16);
+    addTransition(FAState.q16, "o", FAState.q17);
+    addTransition(FAState.q17, "l", FAState.q1);
 
-    addTransition(FAState.q2, "a", FAState.q2);
-    addTransition(FAState.q2, "s", FAState.q2);
-    addTransition(FAState.q2, "o", FAState.q2);
+    addTransition(FAState.q16, "a", FAState.q18);
+    addTransition(FAState.q18, "s", FAState.q19);
+    addTransition(FAState.q19, "o", FAState.q2);
 
-    addTransition(FAState.q2, "o", FAState.q2);
-    addTransition(FAState.q2, "t", FAState.q2);
-    addTransition(FAState.q2, "o", FAState.q2);
-    addTransition(FAState.q2, "r", FAState.q2);
+    addTransition(FAState.q11, "o", FAState.q20);
+    addTransition(FAState.q20, "t", FAState.q21);
+    addTransition(FAState.q21, "o", FAState.q22);
+    addTransition(FAState.q22, "r", FAState.q2);
 
     return [];
   }
